41c712855642611176bd8e3d64ca995762464236
progmamun <mamun333089@gmail.com>
2021-07-15 22:48:02 +0600

    magic methods

---
+++ php-oop/magic_methods.php
@@ -0,0 +1,13 @@
+<?php
+// Automatically run based on condition
+// __construct()
+// they starts with prefix(__)function
+class abc
+{
+    public function first()
+    {
+        echo "This is first function";
+    }
+}
+$test = new abc();
+$test->first();
