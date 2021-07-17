1f0433fb88ba8de841bcdd3bc0432059e3683751
progmamun <mamun333089@gmail.com>
2021-07-17 18:14:00 +0600

    magic constants

---
+++ php-oop/magic_constants.php
@@ -0,0 +1,9 @@
+<?php
+echo "Line number : " . __LINE__ . "<br>";
+echo "FILE path : " . __FILE__;
+echo " folder path : " . __DIR__;
+echo "function name : " . __FUNCTION__;
+echo "class name return : " . __CLASS__;
+echo "method name return : " . __METHOD__;
+echo "namespace name return : " . __NAMESPACE__;
+echo "trait name return : " . __TRAIT__;
