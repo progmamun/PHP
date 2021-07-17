30ce1ccea3849bd1cece1424d64914af48f48f8e
progmamun <mamun333089@gmail.com>
2021-07-17 17:49:42 +0600

    clone method

--- php-oop/clone_method.php
+++ php-oop/clone_method.php
@@ -8,9 +8,30 @@ class student
     {
         $this->name = $n;
     }
+    public function setCourse(course $c)
+    {
+        $this->course = $c;
+    }
+    public function __clone()
+    {
+        $this->course = clone $this->course;
+    }
 }
 
+class course
+{
+    public $cname;
+    public function __construct($cn)
+    {
+        $this->cname = $cn;
+    }
+}
 $student1 = new student('Mamun');
+$course1 = new course('PHP');
+$student1->setCourse($course1);
 $student2 = clone $student1;
 $student2->name = 'Al Mamun Khan';
-echo $student1->name;
+$student2->course->cname = 'Golang';
+print_r($student1);
+print_r($student2);
+// echo $student1->name;
