0e1f1ec44dc9b2e168f9c242aa1f2acaecd729d6
progmamun <mamun333089@gmail.com>
2021-07-17 20:18:23 +0600

    get function oop

---
+++ php-oop/get_function_oop.php
@@ -0,0 +1,13 @@
+<?php
+/*
+1. get_class
+2. get_parent_class
+3. get_class_methods
+4. get_class_vars
+5. get_object_vars
+6. get_called_class
+7. get_declared_classes
+8. get_declared_interfaces
+9. get_declared_traits
+10. class_alias
+ */
